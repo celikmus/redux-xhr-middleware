7a053c77b74e4845eafa5a7de3861815c623c86b
Mustafa Celik <celikmus@gmail.com>
2016-09-27 23:18:28 +0100

    Initial api middleware.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import merge from '../../../node_modules/lodash/merge';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const request = (path, method, body) => {
+
+};
+
+const api = ({dispatch}) => next => action => {
+  const {
+    types,
+    xhr,
+    payload = {}
+  } = action;
+
+  if (!types) {
+    // Normal action: pass it on
+    return next(action);
+  }
+
+  if (!Array.isArray(types) ||
+    types.length !== 3 ||
+    !types.every(type => typeof type === 'string')
+  ) {
+    throw new Error('api: Expected an array of three string types.');
+  }
+
+  if ((typeof xhr !== 'object') || !xhr.url || !xhr.method) {
+    throw new Error('api: Expected xhr to be an object with at least a url and a method.');
+  }
+
+  const [requestType, successType, failType] = types;
+
+  dispatch(merge({}, {
+    type: requestType,
+    payload
+  }));
+
+  const method = xhr.method.toUpperCase();
+  return request(xhr.url, method, xhr.data).then(
+    response => dispatch({
+      type: successType,
+      payload: Object.assign({}, payload, response)
+    }),
+    error => dispatch({
+      type: failType,
+      payload: Object.assign({}, payload, error)
+    }));
+};
+export default api;
