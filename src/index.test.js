c677b37fd66739356ed951b7c6c191bc95cc9e45
Mustafa Celik <celikmus@gmail.com>
2016-10-02 12:12:31 +0100

    Added initial testcases.

---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+import apiMiddleware from './index';
+
+
+describe('api middleware', () => {
+  const doDispatch = () => {};
+  const doGetState = () => {};
+  const nextHandler = apiMiddleware({dispatch: doDispatch, getState: doGetState});
+
+  it('must return a function to handle next', () => {
+    expect(typeof nextHandler).toBe('function');
+    expect(nextHandler.length).toBe(1);
+  });
+
+  describe('handle next', () => {
+    it('must return a function to handle action', () => {
+      const actionHandler = nextHandler();
+      expect(typeof actionHandler).toBe('function');
+      expect(actionHandler.length).toBe(1);
+    });
+  });
+});
