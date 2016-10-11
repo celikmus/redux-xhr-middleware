8f439d2e2cb093d57a7114dcdb4578c8a09bed49
Mustafa Celik <celikmus@gmail.com>
2016-10-11 04:15:12 +0100

    Cleaned up webpack config.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,35 +1,14 @@
-import webpack from 'webpack';
 import path from 'path';
-
 const { NODE_ENV } = process.env;
-
-const plugins = [
-  new webpack.optimize.OccurenceOrderPlugin(),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
-  })
-];
-
 const filename = `redux-xhr.${NODE_ENV}.js`;
 
 export default {
-  module: {
-    loaders: [
-      { test: /\*dex.js?$/,
-        loaders: 'babel',
-        include: path.resolve(__dirname, 'src')
-      }
-    ]
-  },
   entry: [
     'whatwg-fetch',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename,
-    library: 'ReduxXhr',
-    libraryTarget: 'umd'
-  },
-  plugins
+    filename
+  }
 };
