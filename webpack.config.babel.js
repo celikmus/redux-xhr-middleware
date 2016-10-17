12896b5f3acbdcc4006b56e2cc43d17ffe1de456
Mustafa Celik <celikmus@gmail.com>
2016-10-17 21:52:42 +0100

    Fixed build issues.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,14 +1,27 @@
 import path from 'path';
-const { NODE_ENV } = process.env;
-const filename = `redux-xhr.${NODE_ENV}.js`;
+import webpack from 'webpack';
 
 export default {
+  devtool: 'source-map',
   entry: [
     'whatwg-fetch',
-    './src/index'
+    path.join(__dirname, 'src')
   ],
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      include: [path.join(__dirname, 'src')],
+      loader: 'babel'
+    }]
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename
-  }
+    filename: 'index.js',
+    sourceMapFilename: '[file].map',
+    libraryTarget: 'umd',
+    library: 'ReduxXhrMiddleware'
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin()
+  ]
 };
