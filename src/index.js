510e540fb6fa758136e4e4555b082958b9bce13b
Mustafa Celik <celikmus@gmail.com>
2016-10-28 16:02:09 +0100

    Added normalizr handler into the middleware.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import merge from 'lodash/merge';
+import {normalize} from 'normalizr';
 
 const request = (path, method, body, requestOptions = {}) => {
   const {requestHeaders, requestGateway} = requestOptions;
@@ -37,9 +38,10 @@ const request = (path, method, body, requestOptions = {}) => {
 };
 
 const creator = (options) => {
-  const middleware = ({dispatch}) => next => action => {
+  const middleware = ({dispatch, getState}) => next => action => {
     const {
       types,
+      schema,
       xhr,
       payload = {}
     } = action;
@@ -70,10 +72,76 @@ const creator = (options) => {
     const method = xhr.method.toUpperCase();
     return request(xhr.url, method, xhr.data, options)
     .then(response => {
-      dispatch({
-        type: successType,
-        payload: Object.assign({}, payload, response)
-      });
+      if (!schema) {
+        dispatch({
+          type: successType,
+          payload: Object.assign({}, payload, response)
+        });
+      } else {
+        // Normalise response
+        const state = getState();
+        if (method.toUpperCase() === 'DELETE') {
+          const urlParts = xhr.url.split('/');
+          const entityId = urlParts[urlParts.length - 1];
+          const collection = schema.getKey();
+          state.entities[collection][entityId] = null;
+          dispatch(Object.assign({}, {
+            type: successType
+          }));
+        } else {
+          let updatedResponse = response;
+          let isCollection = Array.isArray(response);
+          let hasPayload = !!Object.entries(payload).length;
+          if (hasPayload) {
+            // Does response item/collection have sub collections?
+            // If it does have sub collections, we need to link the entity to those collections,
+            // ie. subcollections need to have the id of this entity, ie. the entity they belong to.
+            //
+            // itemId, ie. key for subcollections, must be specified in the schema:
+            // e.g.
+            // let's say our current entity is 'hotel' with id: 'XYZ',
+            // and 'hotel' entity has 'rooms' subcollection on it.
+            // The schemas for 'hotel' and 'room' entities will look like below:
+            //
+            // const hotelSchema = new Schema('hotels');
+            // const roomSchema = new Schema('rooms');
+            // hotelSchema.define({
+            //   rooms: arrayOf(roomSchema),
+            //   itemId: 'hotelId' // key to populate in rooms
+            // });
+            //
+            // When the above schema setup is processed by the middleware here,
+            // the middleware will add hotelId: 'XYZ' to each zone entry in the 'rooms' subcollection.
+            let itemSchema = schema.getItemSchema ? schema.getItemSchema() : schema;
+            let itemKey = itemSchema.itemId;
+            let sampleItem = isCollection ? response[0] || {} : response;
+            // Get collection props
+            const arrayProps = Object.keys(sampleItem).filter(prop => Array.isArray(sampleItem[prop]));
+            // Insert itemId in sub collection...
+            if (isCollection) {
+              arrayProps.forEach(prop => {
+                if (itemSchema[prop]) {
+                  response.forEach((item) => {
+                    item[prop] = item[prop].map((subItem) => merge({}, subItem, { [itemKey]: item.id }));
+                  });
+                }
+              });
+              updatedResponse = response.map(entry => Object.assign({}, entry, payload));
+            } else {
+              arrayProps.forEach(prop => {
+                if (itemSchema[prop]) {
+                  response[prop] = response[prop].map((subItem) => merge({}, subItem, { [itemKey]: response.id }));
+                }
+              });
+              updatedResponse = Object.assign({}, response, payload);
+            }
+          }
+          dispatch({
+            type: successType,
+            payload: normalize(updatedResponse, schema)
+          });
+        }
+      }
     })
     .catch(
         error => {
